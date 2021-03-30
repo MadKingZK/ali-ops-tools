3d34dcbd841f101de3e014adcf7117ec1221b8d7
zangkuo <zangkuo@xiaoniangao.com>
2021-03-30 10:18:30 +0800

    add update set-snaprule.py

--- disk-snaprule/set-snaprule.py
+++ disk-snaprule/set-snaprule.py
@@ -59,6 +59,15 @@ class DiskSnapTools:
                 snapshot_id = disk.get('AutoSnapshotPolicyId')
                 yield (disk_id, snapshot_id)
 
+    def get_pika_disks_info(self, instance_id, instance_name):
+        if instance_name.endswith('-0') or instance_name.endswith('-1'):
+            result = asyncio.run(self.get_disk_info_async(instance_id))
+            disks_info = (result.to_map().get('body').get('Disks').get('Disk'))
+            for disk in disks_info:
+                disk_id = disk.get('DiskId')
+                snapshot_id = disk.get('AutoSnapshotPolicyId')
+                yield (disk_id, snapshot_id)
+
     @staticmethod
     def csv_reader(csv_file):
         csv_reader = csv.reader(open(csv_file))
@@ -80,20 +89,29 @@ class DiskSnapTools:
 
     # 过滤出快照规则有误或为空的磁盘，调用set_snanp方法设置上快照规则
     def set_nosnap_rule_disk(self, disk_id, snapshot_id, correct_snapid, service_type):
-        if service_type == "db":
-            if snapshot_id != correct_snapid:
-                disk_ids = "['" + disk_id + "']"
-                try:
-                    set_snap_res = asyncio.run(self.set_snanp(correct_snapid, disk_ids))
-                    print(set_snap_res.to_map)
-                except Exception as err:
-                    print("-->", disk_id, err)
+        if snapshot_id != correct_snapid:
+            disk_ids = "['" + disk_id + "']"
+            try:
+                set_snap_res = asyncio.run(self.set_snanp(correct_snapid, disk_ids))
+                print(set_snap_res.to_map)
+            except Exception as err:
+                print("-->", disk_id, err)
 
     # 工具入口
     def run(self, service_type, csv_file, correct_snapid):
-        for instance_id, instance_name in self.csv_reader(csv_file):
-            for disk_id, snapshot_id in self.get_dbs_disks_info(instance_id, instance_name):
-                self.set_nosnap_rule_disk(disk_id, snapshot_id, correct_snapid, service_type)
+        if service_type == "db":
+            for instance_id, instance_name in self.csv_reader(csv_file):
+                for disk_id, snapshot_id in self.get_dbs_disks_info(instance_id, instance_name):
+                    self.set_nosnap_rule_disk(disk_id, snapshot_id, correct_snapid, service_type)
+
+        if service_type == "pika":
+            for instance_id, instance_name in self.csv_reader(csv_file):
+                for disk_id, snapshot_id in self.get_dbs_disks_info(instance_id, instance_name):
+                    self.set_nosnap_rule_disk(disk_id, snapshot_id, correct_snapid, service_type)
+
+
+
+
 
 
 if __name__ == '__main__':
